44d59eb33d1d35c338e8a807b60e2de5a52c5003
Ivan Pidov <ivan.pidov@gmail.com>
2019-01-25 20:43:43 +0200

    chore(qutebrowser): tweak tab colors

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,7 @@
+c.colors.tabs.selected.even.fg = "black"
+c.colors.tabs.selected.even.bg = "#e8c000"
+c.colors.tabs.selected.odd.fg = "black"
+c.colors.tabs.selected.odd.bg = "#e8c000"
 c.tabs.position = "top"
 c.url.searchengines = {
    "DEFAULT": "https://startpage.com/do/asearch?q={}"
