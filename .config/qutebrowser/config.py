bffb13d7ae30c445598a7be8cfcef8028903159c
Ivan Pidov <ivan.pidov@gmail.com>
2019-01-25 20:21:26 +0200

    feat(qutebrowser): change default and start pages to startpage.com

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,3 +2,5 @@ c.tabs.position = "top"
 c.url.searchengines = {
    "DEFAULT": "https://startpage.com/do/asearch?q={}"
 }
+c.url.default_page = "https://startpage.com/"
+c.url.start_pages = "https://startpage.com/"
